eaeef16391b974a5ba4832da982eb816bb084e29
Rosa <rosamolina100@gmail.com>
2022-01-14 14:17:34 +0100

    a

--- src/main/java/org/springframework/samples/petclinic/product/ProductService.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductService.java
@@ -2,22 +2,36 @@ package org.springframework.samples.petclinic.product;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
     public List<Product> getAllProducts(){
-        return null;
+        List<Product> p = productRepository.findAll();
+        return p;
     }
 
     public List<Product> getProductsCheaperThan(double price) {
-        return null;
+        List<Product> p = this.productRepository.findByPriceLessThan(price);
+        return p;
     }
 
     public ProductType getProductType(String typeName) {
-        return null;
+        ProductType tipoProducto = productRepository.findProductType(typeName);
+        return tipoProducto;
     }
 
-    public Product save(Product p){
-        return null;       
+    public void save(Product p){
+        productRepository.save(p);       
     }
 
-    
+    public List<ProductType> findAllProductTypes(){
+        List<ProductType> pt = productRepository.findAllProductTypes();
+        return pt;
+    }
 }
--- src/main/java/org/springframework/samples/petclinic/product/ProductType.java
+++ src/main/java/org/springframework/samples/petclinic/product/ProductType.java
@@ -1,11 +1,27 @@
 package org.springframework.samples.petclinic.product;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+import org.springframework.samples.petclinic.model.BaseEntity;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-public class ProductType {
-    Integer id;
+@Entity
+@Table(name = "product_type")
+public class ProductType extends BaseEntity{
+
+    @NotEmpty
+    @Size(min = 3, max = 50)
+    @Column(name = "name", unique = true)
     String name;
 }
